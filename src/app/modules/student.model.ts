2dbfbaf5fc4faa1b484b1212228a179bab2e6e0b
tuhinhossain123 <csetuhin55@gmail.com>
2024-11-01 23:02:40 +0600

    npm validatior useing

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -5,18 +5,35 @@ import {
   TStudent,
   TUserName,
 } from './student/student.interface';
+import validator from 'validator';
 
 const userNameSchema = new Schema<TUserName>({
   firstName: {
     type: String,
     required: [true, 'First name is required.'],
+    maxlength: [20, 'First name more than 20 characters'],
+    trim: true,
+    validate: {
+      validator: function (value) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not in capitalizen formate',
+    },
   },
   middleName: {
     type: String,
+    trim: true,
   },
   lastName: {
     type: String,
     required: [true, 'Last name is required.'],
+    maxlength: [20, 'Last name more than 20 characters'],
+    trim: true,
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    },
   },
 });
 
@@ -24,26 +41,32 @@ const guardianSchema = new Schema<TGuardian>({
   father: {
     type: String,
     required: [true, 'Father name is required.'],
+    trim: true,
   },
   fatherOccuption: {
     type: String,
     required: [true, 'Father’s occupation is required.'],
+    trim: true,
   },
   fatherContact: {
     type: String,
     required: [true, 'Father’s contact number is required.'],
+    trim: true,
   },
   mother: {
     type: String,
     required: [true, 'Mother name is required.'],
+    trim: true,
   },
   motherOccuption: {
     type: String,
     required: [true, 'Mother’s occupation is required.'],
+    trim: true,
   },
   motherContact: {
     type: String,
     required: [true, 'Mother’s contact number is required.'],
+    trim: true,
   },
 });
 
@@ -51,18 +74,22 @@ const localGuardianSchema = new Schema<TLocalGuardian>({
   name: {
     type: String,
     required: [true, 'Local guardian name is required.'],
+    trim: true,
   },
   occupation: {
     type: String,
     required: [true, 'Local guardian’s occupation is required.'],
+    trim: true,
   },
   contactNo: {
     type: String,
     required: [true, 'Local guardian’s contact number is required.'],
+    trim: true,
   },
   address: {
     type: String,
     required: [true, 'Local guardian’s address is required.'],
+    trim: true,
   },
 });
 
@@ -71,10 +98,12 @@ const studentSchema = new Schema<TStudent>({
     type: String,
     required: [true, 'Student ID is required.'],
     unique: true,
+    trim: true,
   },
   name: {
     type: userNameSchema,
     required: [true, 'Student name is required.'],
+    trim: true,
   },
   gender: {
     type: String,
@@ -83,28 +112,39 @@ const studentSchema = new Schema<TStudent>({
       message: '{VALUE} is not a valid gender option.',
     },
     required: [true, 'Gender is required.'],
+    trim: true,
   },
   dateOfBirth: {
     type: String,
+    trim: true,
   },
   contactNo: {
     type: String,
     required: [true, 'Student contact number is required.'],
+    trim: true,
   },
   email: {
     type: String,
     required: [true, 'Email address is required.'],
     unique: true,
+    trim: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is not valid email type',
+    },
   },
   password: {
     type: String,
     required: [true, 'Student password is required.'],
+    trim: true,
   },
   emargencyContact: {
     type: String,
+    trim: true,
   },
   bloodGroup: {
     type: String,
+    trim: true,
     enum: {
       values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
     },
@@ -112,6 +152,7 @@ const studentSchema = new Schema<TStudent>({
   presetAddress: {
     type: String,
     required: [true, 'Present address is required.'],
+    trim: true,
   },
   permanentAddress: {
     type: String,
@@ -121,13 +162,16 @@ const studentSchema = new Schema<TStudent>({
   guardian: {
     type: guardianSchema,
     required: [true, 'Guardian details are required.'],
+    trim: true,
   },
   localGuardian: {
     type: localGuardianSchema,
     required: [true, 'Local guardian details are required.'],
+    trim: true,
   },
   photoUrl: {
     type: String,
+    trim: true,
   },
   isActive: {
     type: String,
@@ -136,6 +180,7 @@ const studentSchema = new Schema<TStudent>({
       message: '{VALUE} is not a valid status option.',
     },
     default: 'active',
+    trim: true,
   },
   isDeleated: {
     type: Boolean,
